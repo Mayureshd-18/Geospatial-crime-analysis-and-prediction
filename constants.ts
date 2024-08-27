ece440b3f6ee8346dff1aadfd201dd18f88145d6
Mayureshd-18 <mayuresh.dharwadkar211@vit.edu>
2024-08-27 22:33:03 +0530

    changed consts

--- constants.ts
+++ constants.ts
@@ -2,3 +2,9 @@ export const CHILDREN_CSV="assets/csv/children.csv";
 export const ELDER_CSV="../assets/csv/elderly2.csv";
 export const WOMEN_CSV="assets/csv/women.csv";
 export const ELECTION_CSV="assets/csv/ELECTION_data_loc_m_y.csv";
+
+
+export const PREDICTION_MODEL="assets/csv/ELECTION_data_loc_m_y.csv";
+export const CRIMINAL_PREDICTION="assets/csv/ELECTION_data_loc_m_y.csv";
+export const SENTIMENT_ANALYSIS="assets/csv/ELECTION_data_loc_m_y.csv";
+export const RESOURCE_ALLOCATION="https://cid-resource-allocation.streamlit.app/";
